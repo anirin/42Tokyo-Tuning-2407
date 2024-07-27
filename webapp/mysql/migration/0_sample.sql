6ecb46352522c2eaee420c928077c3f93b6fe9cd
anirin <atu8823atu@gmail.com>
2024-07-27 15:43:23 +0900

    indexの追加

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,1 +1,17 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
+-- sessions テーブルにインデックスを追加
+CREATE INDEX idx_session_token ON sessions(session_token);
+
+-- tow_trucks テーブルにインデックスを追加
+CREATE INDEX idx_tow_truck_driver_id ON tow_trucks(driver_id);
+
+-- locations テーブルにインデックスを追加
+CREATE INDEX idx_location_tow_truck_id ON locations(tow_truck_id);
+
+-- dispatchers テーブルにインデックスを追加
+CREATE INDEX idx_dispatcher_user_id ON dispatchers(user_id);
+
+-- orders テーブルにインデックスを追加
+CREATE INDEX idx_order_client_id ON orders(client_id);
+CREATE INDEX idx_order_dispatcher_id ON orders(dispatcher_id);
+CREATE INDEX idx_order_tow_truck_id ON orders(tow_truck_id);
