2842f6c4f924c5fdf78b1d8f36159c2d6d26ce2f
Yuya Oda <yoda@c2r2s7.42tokyo.jp>
2024-07-28 13:11:31 +0900

    a

--- webapp/backend/src/cache/edge_cache.rs
+++ webapp/backend/src/cache/edge_cache.rs
@@ -1,96 +1,97 @@
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc};
+use dashmap::DashMap;
 use lazy_static::lazy_static;
 use crate::models::graph::Edge;
-use std::collections::HashMap;
 use crate::domains::map_service::MapRepository;
 
 #[derive(Clone)]
 pub struct EdgeCache {
-    pub store: [Arc<Mutex<HashMap<i32, Vec<Edge>>>>; 7],
+    pub store: [Arc<DashMap<i32, Vec<Edge>>>; 7],
 }
 
 impl EdgeCache {
     fn new() -> Self {
         EdgeCache {
-			store: [
-				Arc::new(Mutex::new(HashMap::new())),
-				Arc::new(Mutex::new(HashMap::new())),
-				Arc::new(Mutex::new(HashMap::new())),
-				Arc::new(Mutex::new(HashMap::new())),
-				Arc::new(Mutex::new(HashMap::new())),
-				Arc::new(Mutex::new(HashMap::new())),
-				Arc::new(Mutex::new(HashMap::new())),
-			]
+            store: [
+                Arc::new(DashMap::new()),
+                Arc::new(DashMap::new()),
+                Arc::new(DashMap::new()),
+                Arc::new(DashMap::new()),
+                Arc::new(DashMap::new()),
+                Arc::new(DashMap::new()),
+                Arc::new(DashMap::new()),
+            ]
         }
     }
 
-	pub fn get_edges(&self, area_id: usize) -> HashMap<i32, Vec<Edge>> {
-		let store = self.store[area_id - 1].lock().unwrap();
-		store.clone()
-	}
+    pub fn get_edges(&self, area_id: usize) -> DashMap<i32, Vec<Edge>> {
+        let store = self.store[area_id - 1].clone();
+        store.iter().map(|entry| (*entry.key(), entry.value().clone())).collect()
+    }
 
-	pub fn update_edge_weight(&self, area_id: usize, node_a_id: i32, node_b_id: i32, weight: i32) {
-		let mut store = self.store[area_id - 1].lock().unwrap();
-		if let Some(edges) = store.get_mut(&node_a_id) {
+    pub fn update_edge_weight(&self, area_id: usize, node_a_id: i32, node_b_id: i32, weight: i32) {
+        let store = self.store[area_id - 1].clone();
+		if let Some(mut edges) = store.get_mut(&node_a_id) {
 			for edge in edges.iter_mut() {
 				if edge.node_b_id == node_b_id {
 					edge.weight = weight;
 				}
 			}
 		}
-		if let Some(edges) = store.get_mut(&node_b_id) {
+		if let Some(mut edges) = store.get_mut(&node_b_id) {
 			for edge in edges.iter_mut() {
 				if edge.node_b_id == node_a_id {
 					edge.weight = weight;
 				}
 			}
-		}
-	}
+		};
+    }
 }
 
 lazy_static! {
     pub static ref EDGE_CACHE: EdgeCache = {
         let cache = EdgeCache::new();
-
         cache
     };
 }
 
 pub async fn cache_edges<T: MapRepository>(repository: T) {
-	let mut edgeses: [HashMap<i32, Vec<Edge>>; 7] = [
-		HashMap::new(),
-		HashMap::new(),
-		HashMap::new(),
-		HashMap::new(),
-		HashMap::new(),
-		HashMap::new(),
-		HashMap::new(),
-	];
-	for i in 1..7 {
-		let i: usize = i;
-		match repository.get_all_edges(Some(i as i32)).await {
-			Ok(edges) => {
-				for edge in edges {
-					edgeses[i - 1]
-						.entry(edge.node_a_id)
-						.or_default()
-						.push(edge.clone());
-					let reverse_edge = Edge {
-						node_a_id: edge.node_b_id,
-						node_b_id: edge.node_a_id,
-						weight: edge.weight,
-					};
-					edgeses[i - 1]
-						.entry(reverse_edge.node_a_id)
-						.or_default()
-						.push(reverse_edge);
-				}
-			},
-			Err(e) => {
-				println!("Error: {:?}", e);
-			}
-		}
-		let mut store = EDGE_CACHE.store[i - 1].lock().unwrap();
-		*store = edgeses[i - 1].clone();
-	}
+    let mut edgeses: [DashMap<i32, Vec<Edge>>; 7] = [
+        DashMap::new(),
+        DashMap::new(),
+        DashMap::new(),
+        DashMap::new(),
+        DashMap::new(),
+        DashMap::new(),
+        DashMap::new(),
+    ];
+    for i in 1..7 {
+        let i: usize = i;
+        match repository.get_all_edges(Some(i as i32)).await {
+            Ok(edges) => {
+                for edge in edges {
+                    edgeses[i - 1]
+                        .entry(edge.node_a_id)
+                        .or_default()
+                        .push(edge.clone());
+                    let reverse_edge = Edge {
+                        node_a_id: edge.node_b_id,
+                        node_b_id: edge.node_a_id,
+                        weight: edge.weight,
+                    };
+                    edgeses[i - 1]
+                        .entry(reverse_edge.node_a_id)
+                        .or_default()
+                        .push(reverse_edge);
+                }
+            },
+            Err(e) => {
+                println!("Error: {:?}", e);
+            }
+        }
+        let store = EDGE_CACHE.store[i - 1].clone();
+        for entry in edgeses[i - 1].iter() {
+            store.insert(*entry.key(), entry.value().clone());
+        }
+    }
 }
--- webapp/backend/src/models/graph.rs
+++ webapp/backend/src/models/graph.rs
@@ -2,6 +2,7 @@ use sqlx::FromRow;
 use std::collections::{HashMap, BinaryHeap};
 use std::cmp::Ordering;
 use crate::models::tow_truck::TowTruck;
+use dashmap::DashMap;
 
 #[derive(FromRow, Clone, Debug, Default)]
 pub struct Node {
@@ -20,7 +21,7 @@ pub struct Edge {
 #[derive(Debug, Clone)]
 pub struct Graph {
     pub nodes: HashMap<i32, Node>,
-    pub edges: HashMap<i32, Vec<Edge>>,
+    pub edges: DashMap<i32, Vec<Edge>>,
 }
 
 #[derive(Copy, Clone, Eq, PartialEq)]
@@ -49,26 +50,28 @@ impl Graph {
     pub fn new() -> Self {
         Graph {
             nodes: HashMap::new(),
-            edges: HashMap::new(),
+            edges: DashMap::new(),
         }
     }
 
-	pub fn update_edge_weight(&mut self, node_a_id: i32, node_b_id: i32, weight: i32) {
-		if let Some(edges) = self.edges.get_mut(&node_a_id) {
-			for edge in edges.iter_mut() {
-				if edge.node_b_id == node_b_id {
-					edge.weight = weight;
-				}
-			}
-		}
-		if let Some(edges) = self.edges.get_mut(&node_b_id) {
-			for edge in edges.iter_mut() {
-				if edge.node_b_id == node_a_id {
-					edge.weight = weight;
-				}
-			}
-		}
-	}
+    pub fn update_edge_weight(&mut self, node_a_id: i32, node_b_id: i32, weight: i32) {
+        {
+            if let Some(mut edges) = self.edges.get_mut(&node_a_id) {
+                for edge in edges.iter_mut() {
+                    if edge.node_b_id == node_b_id {
+                        edge.weight = weight;
+                    }
+                }
+            }
+        }
+        if let Some(mut edges) = self.edges.get_mut(&node_b_id) {
+            for edge in edges.iter_mut() {
+                if edge.node_b_id == node_a_id {
+                    edge.weight = weight;
+                }
+            }
+        }
+    }
 
     pub fn add_node(&mut self, node: Node) {
         self.nodes.insert(node.id, node);
@@ -91,83 +94,43 @@ impl Graph {
             .push(reverse_edge);
     }
 
-	pub fn find_nearest_tow_truck(&self, tow_trucks: Vec<TowTruck>, from_node_id: i32) -> Option<TowTruck> {
-		let mut distances = HashMap::new();
-		let mut heap = BinaryHeap::new();
-	
-		// Initialize distances
-		for node_id in self.nodes.keys() {
-			distances.insert(*node_id, i32::MAX);
-		}
-		distances.insert(from_node_id, 0);
-	
-		// Push the start node into the heap
-		heap.push(State { cost: 0, position: from_node_id });
-	
-		while let Some(State { cost, position }) = heap.pop() {
-			// if a track is found, return it
-			if let Some(tow_truck) = tow_trucks.iter().find(|t| t.node_id == position) {
-				return Some(tow_truck.clone());
-			}
-	
-			// If the cost is greater than the recorded distance, skip it
-			if cost > *distances.get(&position).unwrap_or(&i32::MAX) {
-				continue;
-			}
-	
-			// Update distances to neighboring nodes
-			if let Some(edges) = self.edges.get(&position) {
-				for edge in edges {
-					let next = State { cost: cost + edge.weight, position: edge.node_b_id };
-	
-					if next.cost < *distances.get(&next.position).unwrap_or(&i32::MAX) {
-						distances.insert(next.position, next.cost);
-						heap.push(next);
-					}
-				}
-			}
-		}
-		return None;
-	}
+    pub fn find_nearest_tow_truck(&self, tow_trucks: Vec<TowTruck>, from_node_id: i32) -> Option<TowTruck> {
+        let mut distances = HashMap::new();
+        let mut heap = BinaryHeap::new();
 
-    // pub fn shortest_path(&self, from_node_id: i32, to_node_id: i32) -> i32 {
-    //     let mut distances = HashMap::new();
-    //     let mut heap = BinaryHeap::new();
+        // Initialize distances
+        for node_id in self.nodes.keys() {
+            distances.insert(*node_id, i32::MAX);
+        }
+        distances.insert(from_node_id, 0);
 
-    //     // Initialize distances
-    //     for node_id in self.nodes.keys() {
-    //         distances.insert(*node_id, i32::MAX);
-    //     }
-    //     distances.insert(from_node_id, 0);
+        // Push the start node into the heap
+        heap.push(State { cost: 0, position: from_node_id });
 
-    //     // Push the start node into the heap
-    //     heap.push(State { cost: 0, position: from_node_id });
+        while let Some(State { cost, position }) = heap.pop() {
+            // if a track is found, return it
+            if let Some(tow_truck) = tow_trucks.iter().find(|t| t.node_id == position) {
+                return Some(tow_truck.clone());
+            }
 
-    //     while let Some(State { cost, position }) = heap.pop() {
-    //         // If we reached the destination node, return the cost
-    //         if position == to_node_id {
-    //             return cost;
-    //         }
+            // If the cost is greater than the recorded distance, skip it
+            if cost > *distances.get(&position).unwrap_or(&i32::MAX) {
+                continue;
+            }
 
-    //         // If the cost is greater than the recorded distance, skip it
-    //         if cost > *distances.get(&position).unwrap_or(&i32::MAX) {
-    //             continue;
-    //         }
+            // Update distances to neighboring nodes
+            if let Some(edges_ref) = self.edges.get(&position) {
+                let edges = edges_ref.value();
+                for edge in edges {
+                    let next = State { cost: cost + edge.weight, position: edge.node_b_id };
 
-    //         // Update distances to neighboring nodes
-    //         if let Some(edges) = self.edges.get(&position) {
-    //             for edge in edges {
-    //                 let next = State { cost: cost + edge.weight, position: edge.node_b_id };
-
-    //                 if next.cost < *distances.get(&next.position).unwrap_or(&i32::MAX) {
-    //                     distances.insert(next.position, next.cost);
-    //                     heap.push(next);
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    //     // If the destination node is unreachable, return i32::MAX
-    //     *distances.get(&to_node_id).unwrap_or(&i32::MAX)
-    // }
+                    if next.cost < *distances.get(&next.position).unwrap_or(&i32::MAX) {
+                        distances.insert(next.position, next.cost);
+                        heap.push(next);
+                    }
+                }
+            }
+        }
+        return None;
+    }
 }
